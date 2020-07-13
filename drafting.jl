e3cf1ce84d8c57216bd2f49b3519d34e9d3ac2c1
Sasha Petrenko <sap625@mst.edu>
2020-07-13 14:15:45 -0500

    Remove drafting code

--- drafting.jl
+++
@@ -1,9 +0,0 @@
-using BenchmarkTools
-a, b = zeros(Float32, 10^7), rand(Float32, 10^7)
-# c_b = @belapsed cmap($sin, $a, $b)
-# cxx_b = @belapsed cxxmap($sin, $a, $b)
-# python_b = @elapsed pymap(sin, a, b);
-# julia_b = @belapsed map!($sin, $a, $b);
-# timings = [python_b, c_b, cxx_b, julia_b]
-julia_b = @belapsed map!($sin, $a, $b);
-timings = [julia_b]
--- src/drafting.jl
+++
@@ -1,9 +0,0 @@
-using AdaptiveResonance
-
-doGreet("Julia")
-
-greet()
-
-for x in 1:2
-    println(x)
-end
