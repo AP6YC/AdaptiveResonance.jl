baaea5783b0138bf2bef6edd1f65012d13e360ba
Sasha Petrenko <sap625@mst.edu>
2020-07-17 10:16:32 -0500

    Minor development and dependencies

--- src/AdaptiveResonance.jl
+++ src/AdaptiveResonance.jl
@@ -1,8 +1,35 @@
 module AdaptiveResonance
 
+using Parameters
+using CUDAapi
+if has_cuda()		# Check if CUDA is available
+    @info "CUDA is on"
+    import CuArrays		# If CUDA is available, import CuArrays
+    CuArrays.allowscalar(false)
+end
+
+
 include("funcs.jl")
 
+
+@with_kw mutable struct Args
+    rho_ub
+    rho_lb
+    alpha
+    beta
+    w_init
+    max_epochs
+    shuffle::Bool = false
+    random_seed::Int64 = 1234.5678
+end
+
 export doGreet, my_f, greet
+
+"""
+    ART()
+
+
+"""
 
 """
     greet()
