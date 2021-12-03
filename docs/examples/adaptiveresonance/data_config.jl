754489508fee782ffe8a81c138f437632f1e741f
Sasha Petrenko <sap625@umsystem.edu>
2021-12-02 20:58:22 -0600

    Begin dataconfig example writing

---
+++ docs/examples/adaptiveresonance/data_config.jl
@@ -0,0 +1,11 @@
+# ---
+# title: ART DataConfig Example
+# id: data_config
+# cover: ../assets/art.png
+# date: 2021-12-2
+# author: "[Sasha Petrenko](https://github.com/AP6YC)"
+# julia: 1.6
+# description: This demo illustrates how the data configuration object works for data preprocessing in ART modules that require it.
+# ---
+
+# ART
