a12c5114dbe41a262102081b62c2753c86f158de
Sasha Petrenko <sap625@umsystem.edu>
2022-10-10 17:39:10 -0500

    Clean up linkage tests

--- test/test_sets.jl
+++ test/test_sets.jl
@@ -140,29 +140,21 @@ end # @testset "kwargs"
 @testset "FuzzyART" begin
     @info "------- FuzzyART Test -------"
 
-    # FuzzyART train and test
+    # FuzzyART initialization and training
     my_FuzzyART = FuzzyART()
-    # local_complement_code = AdaptiveResonance.complement_code(data.train_x)
-    # train!(my_FuzzyART, local_complement_code, preprocessed=true)
     train!(my_FuzzyART, data.train_x)
 
-    # Similarity methods
-    methods = DDVFA_METHODS
-
-    # Both field names
-    field_names = ["T", "M"]
-
     # Compute a local sample for FuzzyART similarity method testing
-    # local_sample = local_complement_code[:, 1]
-    # local_complement_code = AdaptiveResonance.complement_code(data.train_x)
-    # local_sample = data.train_x[:, 1]
-    local_sample = AdaptiveResonance.complement_code(data.train_x[:, 1], config=my_FuzzyART.config)
+    local_sample = complement_code(data.train_x[:, 1], config=my_FuzzyART.config)
 
     # Compute the local activation and match
     AdaptiveResonance.activation_match!(my_FuzzyART, local_sample)
 
-    # Test that every method and field name
-    for method in methods
+    # Both field names
+    field_names = ["T", "M"]
+
+    # Test that every method and field name computes
+    for method in DDVFA_METHODS
         results = Dict()
         for field_name in field_names
             results[field_name] = AdaptiveResonance.similarity(method, my_FuzzyART, field_name, local_sample)
@@ -176,7 +168,6 @@ end # @testset "kwargs"
     @test_throws ErrorException AdaptiveResonance.similarity("asdf", my_FuzzyART, "T", local_sample)
     # Access the wrong output function ("A")
     @test_throws ErrorException AdaptiveResonance.similarity("centroid", my_FuzzyART, "A", local_sample)
-
 end # @testset "FuzzyART"
 
 @testset "ARTSCENE.jl" begin
