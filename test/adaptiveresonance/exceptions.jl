4292cc8ce167bd44d8f6b40613f2dae23452eb73
Sasha Petrenko <sap625@umsystem.edu>
2022-12-01 11:56:56 -0600

    Add init_train! exception test

--- test/adaptiveresonance/exceptions.jl
+++ test/adaptiveresonance/exceptions.jl
@@ -37,11 +37,30 @@ Tests the edge cases and exceptions of the entire `AdaptiveResonance.jl` package
     end
 end
 
+@testset "init_tain!" begin
+    # Create a new FuzzyART module
+    art = FuzzyART()
+
+    # Test that the new module's data config is not setup
+    @test art.config.setup == false
+
+    # Test that initializing training fails if the data is not preprocessed
+    # and the data config is not setup (using the RealVector function)
+    x = rand(2)
+    @test_throws ErrorException AdaptiveResonance.init_train!(x, art, false)
+
+    # Create faulty data and say that it is preprocessed
+    x_cc_bad = rand(3)
+    @test_throws ErrorException AdaptiveResonance.init_train!(x_cc_bad, art, true)
+end
+
 @testset "init_classify!" begin
     # Create a new FuzzyART module
     art = FuzzyART()
+
     # Test that the new module's data config is not setup
     @test art.config.setup == false
+
     # Test that initializing classification fails if the data is not
     # preprocessed and the data config is not setup
     @test_throws ErrorException AdaptiveResonance.init_classify!(rand(2, 2), art, false)
