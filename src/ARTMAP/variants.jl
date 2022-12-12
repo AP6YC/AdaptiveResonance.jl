244ffcbe3329140ec6419dfe5f2452a055cab1e6
Sasha Petrenko <sap625@umsystem.edu>
2022-12-12 15:38:31 -0600

    Fix DAM kwargs syntax

--- src/ARTMAP/variants.jl
+++ src/ARTMAP/variants.jl
@@ -12,5 +12,5 @@ Constructs a Default ARTMAP module using a SFAM module using Default ARTMAP's ch
 1. G. P. Amis and G. A. Carpenter, 'Default ARTMAP 2,' IEEE Int. Conf. Neural Networks - Conf. Proc., vol. 2, no. September 2007, pp. 777-782, Mar. 2007, doi: 10.1109/IJCNN.2007.4371056.
 """
 function DAM(;kwargs...)
-    return SFAM(choice_by_difference=true, kwargs...)
+    return SFAM(;choice_by_difference=true, kwargs...)
 end
