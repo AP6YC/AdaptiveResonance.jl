7b219ec2ff443125d0489b1db036c9aa2792503d
Documenter.jl <documenter@juliadocs.github.io>
2023-05-16 12:38:05 +0000

    build based on 3842137

---
+++ v0.8.2/examples/art/ddvfa_supervised.jl
@@ -0,0 +1,42 @@
+using AdaptiveResonance # ART
+using MLDatasets        # Iris dataset
+using DataFrames        # DataFrames, necessary for MLDatasets.Iris()
+using MLDataUtils       # Shuffling and splitting
+using Printf            # Formatted number printing
+
+# Get the iris dataset
+iris = Iris(as_df=false)
+# Manipulate the features and labels into a matrix of features and a vector of labels
+features, labels = iris.features, iris.targets
+
+labels = convertlabel(LabelEnc.Indices{Int}, vec(labels))
+unique(labels)
+
+(X_train, y_train), (X_test, y_test) = stratifiedobs((features, labels))
+
+art = DDVFA()
+
+# Train in simple supervised mode by passing the labels as a keyword argument.
+y_hat_train = train!(art, X_train, y=y_train)
+println("Training labels: ",  size(y_hat_train), " ", typeof(y_hat_train))
+
+# Classify both ways
+y_hat = AdaptiveResonance.classify(art, X_test)
+y_hat_bmu = AdaptiveResonance.classify(art, X_test, get_bmu=true)
+
+# Check the shape and type of the output labels
+println("Testing labels: ",  size(y_hat), " ", typeof(y_hat))
+println("Testing labels with bmu: ",  size(y_hat_bmu), " ", typeof(y_hat_bmu))
+
+# Calculate performance on training data, testing data, and with get_bmu
+perf_train = performance(y_hat_train, y_train)
+perf_test = performance(y_hat, y_test)
+perf_test_bmu = performance(y_hat_bmu, y_test)
+
+# Format each performance number for comparison
+@printf "Training performance: %.4f\n" perf_train
+@printf "Testing performance: %.4f\n" perf_test
+@printf "Best-matching unit testing performance: %.4f\n" perf_test_bmu
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
