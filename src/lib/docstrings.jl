f13d5e6ac66940c520b10d4ecc86fa9a6e8f18d0
Sasha Petrenko <sap625@umsystem.edu>
2024-03-22 13:09:18 -0500

    Add common docstring prefix

--- src/lib/docstrings.jl
+++ src/lib/docstrings.jl
@@ -46,30 +46,49 @@ $(METHODLIST)
 # COMMON DOCUMENTATION CONSTANTS
 # -----------------------------------------------------------------------------
 
-# Shared options docstring, inserted at the end of `opts_<...>` structs.
+"""
+Docstring prefix denoting that the constant is used as a common docstring element for other docstrings.
+"""
+const _COMMON_DOC = "Common docstring:"
+
+"""
+$(_COMMON_DOC) shared options docstring, inserted at the end of `opts_<...>` structs.
+"""
 const OPTS_DOCSTRING = """
 These options are a [`Parameters.jl`](https://github.com/mauro3/Parameters.jl) struct, taking custom options keyword arguments.
 Each field has a default value listed below.
 """
 
+"""
+$(_COMMON_DOC) shared argument docstring for ART module arguments.
+"""
 const ART_ARG_DOCSTRING = """
 - `art::ARTModule`: the ARTModule module.
 """
 
+"""
+$(_COMMON_DOC) shared argument docstring for the input sample of features.
+"""
 const X_ARG_DOCSTRING = """
 - `x::RealVector`: the sample vector to use.
 """
 
+"""
+$(_COMMON_DOC) shared argument docstring for the weight vector.
+"""
 const W_ARG_DOCSTING = """
 - `W::RealVector`: the weight vector to use.
 """
 
+"""
+$(_COMMON_DOC) shared argument docstring for the index of the weight column.
+"""
 const INDEX_ARG_DOCSTRING = """
 - `index::Integer`: the index of the weight column to use.
 """
 
 """
-Shared arguments string for methods using an ART module, sample 'x', and weight vector 'W'.
+$(_COMMON_DOC) shared arguments string for methods using an ART module, sample 'x', and weight vector 'W'.
 """
 const ART_X_W_ARGS = """
 # Arguments
@@ -79,7 +98,7 @@ $(W_ARG_DOCSTING)
 """
 
 """
-Shared arguments string for functions updating a column in a matrix.
+$(_COMMON_DOC) shared arguments string for functions updating a column in a matrix.
 """
 const MATRIX_REPLACE_ARGS_DOCSTRING = """
 # Arguments
@@ -87,12 +106,3 @@ const MATRIX_REPLACE_ARGS_DOCSTRING = """
 - `vec::RealVector`: the vector to put in the matrix at the column index.
 - `index::Integer`: the column index to put the vector.
 """
-
-
-# const ART_X_W_ARGS = """
-# # Arguments
-# - `art::ARTModule`: the ARTModule module.
-# - `x::RealVector`: the sample to use.
-# - `W::ARTMatrix`: the weight matrix to use.
-# - `index::Integer`: the index of the weight to use.
-# """
